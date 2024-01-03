4d163eb8e87a6ca249d97899543529dca55ebef2
Phong.NT216868@sis.hust.edu.vn <Phong.NT216868@sis.hust.edu.vn>
2024-01-03 18:16:46 +0700

    Info user, PMQL

--- BTL_OOP_N17/InfoUser11.Designer.cs
+++ BTL_OOP_N17/InfoUser11.Designer.cs
@@ -22,14 +22,14 @@ namespace BTL_OOP_N17 {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class InfoUser {
+    internal class InfoUser11 {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal InfoUser() {
+        internal InfoUser11() {
         }
         
         /// <summary>
@@ -39,7 +39,7 @@ namespace BTL_OOP_N17 {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("BTL_OOP_N17.InfoUser", typeof(InfoUser).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("BTL_OOP_N17.InfoUser1", typeof(InfoUser11).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- BTL_OOP_N17/Phanmemquanlythietbi11.Designer.cs
+++ BTL_OOP_N17/Phanmemquanlythietbi11.Designer.cs
@@ -22,14 +22,14 @@ namespace BTL_OOP_N17 {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Phanmemquanlythietbi {
+    internal class Phanmemquanlythietbi11 {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Phanmemquanlythietbi() {
+        internal Phanmemquanlythietbi11() {
         }
         
         /// <summary>
@@ -39,7 +39,7 @@ namespace BTL_OOP_N17 {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("BTL_OOP_N17.Phanmemquanlythietbi", typeof(Phanmemquanlythietbi).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("BTL_OOP_N17.Phanmemquanlythietbi1", typeof(Phanmemquanlythietbi11).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
